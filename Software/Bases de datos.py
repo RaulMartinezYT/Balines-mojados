a4f8d0115f350c24e9c4d645b6b8664734193db0
Raul <ytraulmartinez@gmail.com>
2024-08-21 02:08:13 -0300

    8.0

--- Software/Bases de datos.py
+++ Software/Bases de datos.py
@@ -1,5 +1,4 @@
-import sqlite3, os
-from datetime import datetime
+import sqlite3, os,time
 
 # Esta funcion recibe un parametro que va a ser el nombre de la base de datos y devolverá el nombre "limpio" para prevenir errores de tipografia.
 # El parametro es un str al entrar y salir, se lo escribe a minuscula, se le remplazan los "-" con "_" y los espacios en blanco por "_".
@@ -11,9 +10,9 @@ def limpiar_str(a):
     return a
 
 # Este metodo se utiliza para la creacion de bases de datos, es necesario pasarle de parametro el nombre de la base de datos.
-# Le solicitamos al usuario el nombre de la base de datos a crear, una vez lo tenemos lo "limpiamos", una vez limpiado el nombre, chequeamos que exista ese archivo.
-# Si el archivo existe se lo haremos saber al usuario y queda en el si lo remplaza (se eliminará el archivo existente y se creará el nuevo)
-# Si el archivo no existe se crea y listo
+# Chequeamos que exista ese archivo.
+# Si el archivo existe se lo haremos saber al usuario y queda en el si lo remplaza (se eliminará el archivo existente y se creará el nuevo).
+# Si el archivo no existe se crea y listo.
 def crear_db(a):
     try:
         if open(f"db/{a}"):
@@ -22,55 +21,46 @@ def crear_db(a):
             if seleccion != "y" and seleccion != "n":
                 print("Seleccion incorrecta")
             else:
-                if seleccion == "y":
+                if seleccion == "y": 
                     os.remove(f"db/{a}")
                     with open(f"db/{a}", 'w+') as file:
-                        file.write( str(datetime.now()) )
+                        file.close()
                 else:
                     print("No se remplazo la base de datos")
-
-
 
     except FileNotFoundError:
         try:
             with open(f"db/{a}", 'w+') as file:
-                file.write( str(datetime.now()) )
+                file.close()
 
         except Exception as e:
             print(f'No fue posible crear el archivo {e}')
     
 
-# Este metodo se encarga de eliminar archivos
-def eliminar_db():
-    nombre_db = input("Nombre de la base de datos a eliminar:\n")
-    nombre_db = limpiar_str(nombre_db)
+# Este metodo se recibe el nombre de la base de datos deseada para ser eliminada.
+def eliminar_db(a):
     try:
-        os.remove(f"db/{nombre_db}")
+        os.remove(f"db/{a}")
 
     except FileNotFoundError:
         print("El archivo no existe")
     
-
-def usar_db():
-    nombre_db = input("Nombre de la base de datos a usar:\n")
-    nombre_db = limpiar_str(nombre_db)
+# 
+def conectar_db(a):
     try:
-        if open(f"db/{nombre_db}"):
+        if open(f"db/{a}"):
             print("archivo encontrado")
-        
-        else:
-            print("archivo no encontrado")
+            conexion = sqlite3.connect(f"db/{a}")
 
-    except FileNotFoundError:
-        print(f"Error archivo no encontrado")
+    except FileNotFoundError as a:
+        print(f"Error {a}")
 
 # Funcion principal del programa, esta funcion se encarga de saber que quiere realizar el usuario, en este caso el empleado que está utilizando el programa
 def seleccion():
-    seleccion = int(input("1. Crear Base de Datos\n2. Eliminar Base de Datos\n3. Usar una base de datos\n4. Modificar Base de Datos\n"))
+    seleccion = int(input("1. Crear Base de Datos\n2. Modificar Base de Datos\n3. Eliminar Base de Datos\n"))
     
-#    if seleccion != 1 and seleccion != 2 and seleccion != 3 and seleccion != 4:
-#        print("Opcion incorrecta")
-#        seleccion()
+    if seleccion != 1 and seleccion != 2 and seleccion != 3:
+        print("Opcion incorrecta")
 
     if seleccion == 1:
         nombre_db = str(input("Nombre de la base de datos a crear:\n"))
@@ -78,13 +68,15 @@ def seleccion():
         crear_db(nombre_db)
     
     if seleccion == 2:
-        eliminar_db()
+        nombre_db = input("Nombre de la base de datos a usar:\n")
+        nombre_db = limpiar_str(nombre_db)
+        conectar_db(nombre_db)
+        
     
     if seleccion == 3:
-        usar_db()
-
-    if seleccion == 4:
-        print("accion 4")
+        nombre_db = input("Nombre de la base de datos a eliminar:\n")
+        nombre_db = limpiar_str(nombre_db)
+        eliminar_db(nombre_db)
 
 # Main:
 while True:
