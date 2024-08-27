7eed5108574ef65a9072f76f2766d997acb7ef17
Raul <ytraulmartinez@gmail.com>
2024-08-27 09:20:01 -0300

    8.0

--- Software/Bases de datos.py
+++ Software/Bases de datos.py
@@ -16,25 +16,28 @@ def limpiar_str(a):
 def crear_db(a):
     try:
         if open(f"db/{a}"):
-            seleccion = str(input("La base de datos ya existe.\n¿Desea remplazarla? y/n\n"))
+            seleccion = str(input("La base de datos ya existe.\n+======================+\n| ¿Deseas remplazarla? |\n+=======+=======+======+\n        | Y / N |\n        +=======+\n"))
             limpiar_str(seleccion)
             if seleccion != "y" and seleccion != "n":
-                print("Seleccion incorrecta")
+                print("+========================+\n| Seleccion incorrecta |\n+========================+\n")
             else:
                 if seleccion == "y": 
                     os.remove(f"db/{a}")
                     with open(f"db/{a}", 'w+') as file:
                         file.close()
+                    print("+====================+\n| Archivo remplazado |\n+====================+\n")
+
                 else:
-                    print("No se remplazo la base de datos")
+                    print("+=================================+\n| No se remplazo la base de datos |\n+=================================+\n")
 
     except FileNotFoundError:
         try:
             with open(f"db/{a}", 'w+') as file:
                 file.close()
+                print("+================+\n| Archivo creado |\n+================+\n")
 
         except Exception as e:
-            print(f'No fue posible crear el archivo {e}')
+            print(f'[ No fue posible crear el archivo ( {e} ) ]\n')
     
 
 # Este metodo se recibe el nombre de la base de datos deseada para ser eliminada.
@@ -43,27 +46,27 @@ def eliminar_db(a):
         os.remove(f"db/{a}")
 
     except FileNotFoundError:
-        print("El archivo no existe")
+        print("\n+=======================+\n| Archivo no encontrado |\n+=======================+\n")
     
 # 
 def conectar_db(a):
     try:
         if open(f"db/{a}"):
-            print("archivo encontrado")
+            print("+====================+\n| archivo encontrado |\n+====================+\n")
             conexion = sqlite3.connect(f"db/{a}")
 
-    except FileNotFoundError as a:
-        print(f"Error {a}")
+    except FileNotFoundError:
+        print("\n| Archivo no encontrado |\n  ")
 
 # Funcion principal del programa, esta funcion se encarga de saber que quiere realizar el usuario, en este caso el empleado que está utilizando el programa
 def seleccion():
-    seleccion = int(input("1. Crear Base de Datos\n2. Modificar Base de Datos\n3. Eliminar Base de Datos\n"))
+    seleccion = int(input("+============================+\n| 1. Crear Base de Datos     |\n+============================+\n| 2. Modificar Base de Datos |\n+============================+\n| 3. Eliminar Base de Datos  |\n+============================+\n"))
     
     if seleccion != 1 and seleccion != 2 and seleccion != 3:
-        print("Opcion incorrecta")
+        print("+===================+\n| Opcion incorrecta |\n+===================+\n")
 
     if seleccion == 1:
-        nombre_db = str(input("Nombre de la base de datos a crear:\n"))
+        nombre_db = str(input("+=====================================+\n| Nombre de la base de datos a crear: |\n+=====================================+\n"))-
         nombre_db = limpiar_str(nombre_db)
         crear_db(nombre_db)
     
@@ -71,7 +74,6 @@ def seleccion():
         nombre_db = input("Nombre de la base de datos a usar:\n")
         nombre_db = limpiar_str(nombre_db)
         conectar_db(nombre_db)
-        
     
     if seleccion == 3:
         nombre_db = input("Nombre de la base de datos a eliminar:\n")
